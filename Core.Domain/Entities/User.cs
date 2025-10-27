b0194f6f0a196264f47e91ce5c69f5303d55b5d8
Francesco_1974 <francescocolombo36@gmail.com>
2025-10-27 14:42:14 -0300

    Implement base repository

--- Core.Domain/Entities/User.cs
+++ Core.Domain/Entities/User.cs
@@ -1,9 +1,10 @@
 using System;
 using Core.Domain.Enums;
+using Core.Domain.Interfaces;
 
 namespace Core.Domain.Entities;
 
-public class User
+public class User : IEntity
 {
     public Guid Id { get; set; }
     public required string Email { get; set; }
---
+++ Core.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Core.Domain.Interfaces;
+
+namespace Infrastructure.Data.Repositories
+{
+    public interface IBaseRepository<T> where T : class, IEntity
+    {
+        Task<T> CreateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<IEnumerable<T>?> ReadAsync();
+        Task<T?> ReadByIdAsync(Guid id);
+        Task<T?> UpdateAsync(T entity);
+    }
+}
