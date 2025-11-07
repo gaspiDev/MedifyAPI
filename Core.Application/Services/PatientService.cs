bf7c9370ca42a616807805b2ba30bf0b35b1fc39
Francesco_1974 <francescocolombo36@gmail.com>
2025-11-07 10:48:48 -0300

    Application Scaffolding w/ basic Services Mapping and DTOs

---
+++ Core.Application/Services/PatientService.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Services
+{
+    public class PatientService
+    {
+        private readonly IPatientRepository _patientRepository;
+        public PatientService(IPatientRepository patientRepository)
+        {
+            _patientRepository = patientRepository;
+        }
+    }
+}
