10cf4393f1a459fe280a7f13d377543d7b7a2519
Francesco_1974 <francescocolombo36@gmail.com>
2025-11-07 21:56:05 -0300

    update interfaces in repositories

--- Core.Domain/Interfaces/IDoctorRepository.cs
+++ Core.Domain/Interfaces/IDoctorRepository.cs
@@ -4,7 +4,8 @@ namespace Infrastructure.Data.Repositories
 {
     public interface IDoctorRepository
     {
-        Task<IEnumerable<Doctor>> GetAllWithUsersAsync();
-        Task<Doctor?> GetByLicenseAsync(string licenseNumber);
+        Task<IEnumerable<Doctor>> ReadAllWithUsersAsync();
+        Task<Doctor?> ReadByLicenseAsync(string licenseNumber);
+        Task<int?> DeleteDoctor(Guid id);
     }
 }
